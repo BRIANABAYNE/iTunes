0a58b2000fffd656483b00a06393b13d6ae283e8
Briana Bayne <brianabayne@brianas-imac.lan>
2023-08-22 15:01:26 -0600

    Added stroyboards, labels and basic constraints.

---
+++ iTunes/iTunes/AlbumDetails/AlbumDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumDetailViewController.swift
+//  iTunes
+//
+//  Created by Briana Bayne on 8/22/23.
+//
+
+import UIKit
+
+class AlbumDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
